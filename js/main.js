2e3c94280adfd9c5476adff249405d78ea79e78a
Valerchixx <95089858+Valerchixx@users.noreply.github.com>
2021-12-11 18:04:26 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// scroll
+$("[data-scroll]").on('click', function(event){
+    event.preventDefault();
+     
+    let elemid = $(this).data('scroll');
+    let elemOffset = $(elemid).offset().top;
+    $('html,body').animate({
+        scrollTop: elemOffset - 150
+    });
+
+});
+
+$("[data-Top]").on('click', function(event){
+    event.preventDefault();
+     
+    //let elemid = $(this).data('scroll');
+    //let elemOffset = $(elemid).offset().top;
+    $('html,body').animate({
+        scrollTop: 0
+    });
+
+});
+
+// fixed  header
+window.onscroll = function(){
+    let header = document.getElementById('header');
+    if(window.pageYOffset > 400){
+        header.classList.add('fixed')
+    } else{
+        header.classList.remove('fixed')
+    }
+}
+
+
+
+
+// carousel
+$(document).ready(function(){
+    let owl = $(".owl-carousel");
+    owl.owlCarousel({
+        items: 1,
+        margin: 20,
+    });
+    $('.next').click(() => owl.trigger('next.owl.carousel'))
+
+    $('.prev').click(() => owl.trigger('prev.owl.carousel'))
+    
+  });
+
+// burger
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.res-menu');
+
+burger.addEventListener('click', function(){
+    menu.classList.toggle('show');
+
+});
+
+
